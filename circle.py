9dc9b0d0e355cc6d79bab8a5687886e9e569b8a3
Henrique de Lima <122651742+HenriDevRick@users.noreply.github.com>
2024-05-10 15:20:24 -0300

    circle.py

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 # Ask and Collect radius
-radius = float(input("What is the radius of the circle?"))
+radius = float(input("What is the radius of the circle? "))
 
 # Calculate diameter (2 * radius)
 diameter = 2 * radius
@@ -12,7 +12,5 @@ circumference = 2 * pi * radius
 area = pi * radius ** 2
 
 # Print results
-print("Diameter:", diameter)
-print("Circumference:", circumference)
-print("Area:", area)
-print("Thank you for using the circle calculator!")
+print("What is the radius of the circle?",radius)
+print(f"A circle with a radius of {radius} units will have a diameter of {diameter} units, a circumference of {circumference:.2f} units and an area of {area:.2f} square units.")
